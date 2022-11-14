580d56a23046cb3260589f67be0520fba35dcd33
joyakawamoto <joyakawamoto@gmail.com>
2022-11-14 22:00:01 +0900

    Initial Commit

---
+++ browserApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  browserApp
+//
+//  Created by 河本穣也 on 2022/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ browserApp/browserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserAppApp.swift
+//  browserApp
+//
+//  Created by 河本穣也 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct browserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
